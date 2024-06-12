6c2bcfd2af8d9f01359a60fc5dd6d17019be3c79
Landry Dupont <landrydupont790@gmail.com>
2024-06-12 14:48:50 +0200

    react4

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import PokemonCard from './components/pokemonCard';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   return (
     <>
-      <figure>
-        <img 
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-        alt="Bulbasaur" 
-        />
-        <figcaption>
-            Bulbasaur
-        </figcaption>
-      </figure>;
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+  </div>
     </>
-  )
+    )
 }
 
 export default App
--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,35 +1,25 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
+
+
+function PokemonCard({ pokemon }) {
+  return ( 
   
+      <figure>
+          {pokemon.imgSrc ? (
+              <img 
+                  src={pokemon.imgSrc} 
+                  alt={pokemon.name}  
+              />
+              ) : (<p>???</p>
+              )
+          }
 
-function PokemonCard() {
-    const pokemon = pokemonList[1]
-    return ( 
-    
-        <figure>
-            {pokemon.imgSrc ? (
-                <img 
-                    src={pokemon.imgSrc} 
-                    alt={pokemon.name}  
-                />
-                ) : (<p>???</p>
-                )
-            }
+          <figcaption>
+          {pokemon.name}
+          </figcaption>
+  </figure>
+  )
+}
 
-            <figcaption>
-            {pokemon.name}
-            </figcaption>
-    </figure>
-    )
-  }
 
-  
-  export default PokemonCard;
+export default PokemonCard;
