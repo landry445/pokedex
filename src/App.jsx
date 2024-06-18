c58589417e076ee38fdff2ceccd6f5d106afe0c3
Landry Dupont <landrydupont790@gmail.com>
2024-06-18 21:26:23 +0200

    state.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import PokemonCard from './components/pokemonCard.jsx';
-
+import Navbar from './components/NavBar.jsx';
 
 
 
 
 function App() {
-  const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
-  const [pokemonIndex , setpokemonIndex ] = useState(0)
- 
-  const handlePrevious = () => {
-    if (pokemonIndex > 0) {
-      setpokemonIndex(pokemonIndex - 1);
-    }
-  };
-
-  const handleNext = () => {
-    if (pokemonIndex < pokemonList.length - 1) {
-      setpokemonIndex(pokemonIndex + 1);
-    }
-  };
-
+  
   return (
     <>
-    <div>
-      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-  </div>
-  <div>
-    <p>{setpokemonIndex}</p>
-    <button onClick={handlePrevious}>Précédent</button>
-  </div>
-  <div>
-    <p>{setpokemonIndex}</p>
-    <button onClick={handleNext}>Suivant</button>
-  </div>
+   <Navbar/>
     </>
     )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import PokemonCard from './pokemonCard.jsx';
+
+
+
+
+
+function Navbar() {
+  const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+  const [pokemonIndex , setpokemonIndex ] = useState(0)
+ 
+  const handlePrevious = () => {
+    if (pokemonIndex > 0) {
+      setpokemonIndex(pokemonIndex - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setpokemonIndex(pokemonIndex + 1);
+    }
+  };
+
+  return (
+    <>
+    <div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+  </div>
+  <div>
+    <p>{setpokemonIndex}</p>
+    <button onClick={handlePrevious}>Précédent</button>
+  </div>
+  <div>
+    <p>{setpokemonIndex}</p>
+    <button onClick={handleNext}>Suivant</button>
+  </div>
+    </>
+    )
+}
+
+export default Navbar
