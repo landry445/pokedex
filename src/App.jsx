c48a4eee1d0ca432e657449b12f516979f25c3df
Landry Dupont <landrydupont790@gmail.com>
2024-06-19 09:03:37 +0200

    rendering-list.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
 import { useState } from "react";
-
 import NavBar from "./components/NavBar";
 import PokemonCard from "./components/PokemonCard";
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,22 +2,14 @@
 import PropTypes from "prop-types";
 
 function NavBar({ pokemonIndex, setPokemonIndex, pokemonList }) {
-  const pickPrevious = () => setPokemonIndex(pokemonIndex - 1);
-
-  const pickNext = () => setPokemonIndex(pokemonIndex + 1);
 
   return (
     <nav>
-      {pokemonIndex > 0 && (
-        <button type="button" onClick={pickPrevious}>
-          previous
+      {pokemonList.map((pokemon, index) => (
+        <button key={index} onClick={() => setPokemonIndex(index)}>
+          {pokemon.name}
         </button>
-      )}
-      {pokemonIndex < pokemonList.length - 1 && (
-        <button type="button" onClick={pickNext}>
-          next
-        </button>
-      )}
+      ))}
     </nav>
   );
 }
